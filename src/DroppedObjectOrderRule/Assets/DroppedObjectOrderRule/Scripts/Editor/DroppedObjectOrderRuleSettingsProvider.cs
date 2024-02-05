c4b0df22165b2259f7c16a410f9807cf4a112511
DAIKI <daiki.evilone@gmail.com>
2024-02-05 14:59:32 +0900

    Set keywords and use ChangeCheckScope.

--- src/DroppedObjectOrderRule/Assets/DroppedObjectOrderRule/Scripts/Editor/DroppedObjectOrderRuleSettingsProvider.cs
+++ src/DroppedObjectOrderRule/Assets/DroppedObjectOrderRule/Scripts/Editor/DroppedObjectOrderRuleSettingsProvider.cs
@@ -6,11 +6,12 @@ namespace DroppedObjectOrderRule
     public class DroppedObjectOrderRuleSettingsProvider : SettingsProvider
     {
         private const string SettingPath = "Project/DroppedObjectOrderRule";
+        private static readonly string[] Keywords = { "Order", "Sibling" };
 
         [SettingsProvider]
         public static SettingsProvider CreateProvider()
         {
-            return new DroppedObjectOrderRuleSettingsProvider(SettingPath, SettingsScope.Project, null);
+            return new DroppedObjectOrderRuleSettingsProvider(SettingPath, SettingsScope.Project, Keywords);
         }
 
         public DroppedObjectOrderRuleSettingsProvider(string path, SettingsScope scopes, IEnumerable<string> keywords) : base(path, scopes, keywords)
@@ -19,12 +20,15 @@ namespace DroppedObjectOrderRule
 
         public override void OnGUI(string searchContext)
         {
-            EditorGUI.BeginChangeCheck();
-            var settings = DroppedObjectOrderRuleSettings.instance;
-            settings.Enable = EditorGUILayout.Toggle("Enable", settings.Enable);
-            if (EditorGUI.EndChangeCheck())
+            using (var check = new EditorGUI.ChangeCheckScope())
             {
-                settings.Save();
+                var settings = DroppedObjectOrderRuleSettings.instance;
+                settings.Enable = EditorGUILayout.Toggle("Enable", settings.Enable);
+
+                if (check.changed)
+                {
+                    settings.Save();
+                }
             }
         }
     }
